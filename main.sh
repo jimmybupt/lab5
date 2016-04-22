75b4a95df6c4356a7c359241c5d7e03eb3494488
Your Name <you@example.com>
2016-04-22 04:28:26 -0400

    .

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
-./apriori -tr -s30 -c40 -Rappearance0 train0 -
-./apriori -tr -s30 -c40 -Rappearance1 train1 -
-./apriori -tr -s30 -c40 -Rappearance2 train2 -
-./apriori -tr -s30 -c40 -Rappearance3 train3 -
-./apriori -tr -s30 -c40 -Rappearance4 train4 -
+python main.py
+./apriori -tr -s30 -c40 -Rappearance0 train0 rule0
+./apriori -tr -s30 -c40 -Rappearance1 train1 rule1
+./apriori -tr -s30 -c40 -Rappearance2 train2 rule2
+./apriori -tr -s30 -c40 -Rappearance3 train3 rule3
+./apriori -tr -s30 -c40 -Rappearance4 train4 rule4
 
