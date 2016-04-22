6a410833b5e8adfc37b31a3097492ec22f3810a2
Your Name <you@example.com>
2016-04-22 04:23:36 -0400

    main.sh

---
+++ main.sh
@@ -0,0 +1,6 @@
+./apriori -tr -s30 -c40 -Rappearance0 train0 -
+./apriori -tr -s30 -c40 -Rappearance1 train1 -
+./apriori -tr -s30 -c40 -Rappearance2 train2 -
+./apriori -tr -s30 -c40 -Rappearance3 train3 -
+./apriori -tr -s30 -c40 -Rappearance4 train4 -
+
